0e74796822352f6e1d5b827b78c131354703775e
ManongJu <469863752@qq.com>
2016-08-15 17:31:37 +0800

    add setting

--- app/src/main/java/com/github/baby/owspace/view/activity/SettingsActivity.java
+++ app/src/main/java/com/github/baby/owspace/view/activity/SettingsActivity.java
@@ -6,11 +6,20 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.github.baby.owspace.BuildConfig;
 import com.github.baby.owspace.R;
+
+import java.io.File;
+import java.text.DecimalFormat;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Mr.Yangxiufeng
@@ -20,6 +29,20 @@ import butterknife.ButterKnife;
 public class SettingsActivity extends AppCompatActivity {
     @Bind(R.id.toolBar)
     Toolbar toolBar;
+    @Bind(R.id.push_toggle)
+    Switch pushToggle;
+    @Bind(R.id.cacheSize)
+    TextView cacheSize;
+    @Bind(R.id.cacheLayout)
+    RelativeLayout cacheLayout;
+    @Bind(R.id.about)
+    RelativeLayout about;
+    @Bind(R.id.feedback)
+    RelativeLayout feedback;
+    @Bind(R.id.version_tv)
+    TextView versionTv;
+    @Bind(R.id.checkUpgrade)
+    RelativeLayout checkUpgrade;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,5 +59,53 @@ public class SettingsActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        versionTv.setText(BuildConfig.VERSION_NAME);
+        File file = Glide.getPhotoCacheDir(this);
+        DecimalFormat fnum = new DecimalFormat("##0.00");
+        String dd = fnum.format(getDirSize(file));
+        cacheSize.setText(dd + "M");
+    }
+
+    private float getDirSize(File file) {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File[] children = file.listFiles();
+                float size = 0;
+                for (File f : children) {
+                    size += getDirSize(f);
+                }
+                return size;
+            } else {
+                float size = (float) file.length() / 1024 / 1024;
+                return size;
+            }
+        } else {
+            return 0.0f;
+        }
+    }
+
+    @OnClick(R.id.cacheLayout)
+    public void onClick() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Glide.get(getApplication()).clearDiskCache();
+            }
+        }).start();
+        cacheSize.setText("0.00M");
+    }
+
+    @OnClick({R.id.push_toggle, R.id.about, R.id.feedback, R.id.checkUpgrade})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.push_toggle:
+                break;
+            case R.id.about:
+                break;
+            case R.id.feedback:
+                break;
+            case R.id.checkUpgrade:
+                break;
+        }
     }
 }
